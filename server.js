5861ccb467c537b290b4d77ae92ced9f674a9a26
yuxinyi <yuxinyi@meituan.com>
2023-08-29 19:26:16 +0800

    test: 1

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const http = require('http');
 const exec = require('child_process').exec;
 
 http.createServer(function (req, res) {
-  console.log(`exec deploy.sh`);
   exec('sh /code/blog/deploy.sh', function(error, stdout, stderr) {
     if (error) {
       console.error(`exec error: ${error}`);
