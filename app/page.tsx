812073be88c9d68d97650fcd328c6976142f7a64
yuxinyi <yuxinyi@meituan.com>
2025-02-08 20:00:24 +0800

    chore: 测试

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      This is my home
+      This is my home111
     </main>
   )
 }
